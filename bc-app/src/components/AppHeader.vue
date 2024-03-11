e94b1683b77504c37a6db3c4801c5c6b805eaf7b
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-03-11 16:12:45 +0100

    Theme buttons added

--- bc-app/src/components/AppHeader.vue
+++ bc-app/src/components/AppHeader.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+function theme(event) {
+    alert('Hallo');
+}
+</script>
+
 <template>
     <header>
         <nav class="navbar bg-dark-subtle" aria-label="Fahrrad Konfigurator">
@@ -6,6 +14,10 @@
                     <h1 class="display-6"><i class="bi bi-bicycle me-3"></i>Fahrrad Konfigurator</h1>
                 </a>
                 <form class="d-flex">
+                    <div class="btn-group me-2" role="group" aria-label="Basic example">
+                        <button type="button" @click="theme" class="btn btn-outline-secondary active">Dark</button>
+                        <button type="button" class="btn btn-outline-secondary">Light</button>
+                    </div>
                     <button type="button" class="btn btn-outline-secondary me-2"
                         aria-label="Exportieren">Exportieren</button>
                     <button type="button" class="btn btn-outline-secondary" aria-label="Laden">Laden</button>
