dcc65532de991b80b4c419ec2a1e3dc2c6fc0e81
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-03-11 16:14:03 +0100

    Price added

--- bc-app/src/components/module/ConfiguratorModuleItemProduct.vue
+++ bc-app/src/components/module/ConfiguratorModuleItemProduct.vue
@@ -16,13 +16,17 @@ const props = defineProps({
             <button type="button" class="btn btn-sm btn-outline-light" aria-label="Editieren"><i
                     class="bi bi-pencil"></i></button>
         </div>-->
-        <div class="row justify-content-between">
-            <div class="col-10"><a href="#" class="link-light link-underline-secondary link-offset-1">{{ Product || "Product empty" }}</a></div>
-            <div class="col-2"> <button type="button" class="btn btn-sm btn-outline-light" aria-label="Editieren"><i
-                    class="bi bi-pencil"></i></button></div>
+
+        <div class="row">
+            <div class="col-10"><a href="#" class="link-light link-underline-secondary link-offset-1">{{ Product ||
+                "Product empty" }}</a></div>
+            <div class="col-2 text-end"> <button type="button" class="btn btn-sm btn-outline-light" aria-label="Editieren"><i
+                        class="bi bi-pencil"></i></button></div>
         </div>
-        <div class="row justify-content-between">
-            <div class="col-10 text-body-tertiary">{{ Category || "Category empty" }}</div>
+        <div class="row">
+            <div class="col-9 text-body-tertiary">{{ Category || "Category empty" }}</div>
+            <div class="col-3 text-end text-body-tertiary">150,00€</div>
         </div>
+
     </li>
 </template>
