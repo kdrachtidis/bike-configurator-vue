70c8ff52c7d0c6bd4e2b575b852c527071058eac
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-03-12 13:32:59 +0100

    checking JSON import

--- bc-app/src/App.vue
+++ bc-app/src/App.vue
@@ -3,10 +3,10 @@ import AppHeader from './components/AppHeader.vue'
 import ConfiguratorHeader from './components/ConfiguratorHeader.vue'
 import ConfiguratorProductList from './components/basket/ConfiguratorProductList.vue'
 import ConfiguratorModule from './components/module/ConfiguratorModule.vue'
+import bicycle from './components/bicycle.vue'
 
 const ModuleItemCategory = ["Adapter-Kits", "Cleats & Schuhplatten", "Fahrradkassetten", "Fahrradketten", "Fahrradkurbeln", "Innenlager", "Kettenblätter", "Kettenführungen & Bashguards", "Kleinteile", "Nabenschaltung", "Pedale", "Riemen", "Riemenscheiben", "Singlespeed", "Verschleißsets"]
 
-const ModuleGroup = ["Antrieb", "Cockpit", "Dämpfer", "Fahrradbremsen", "Fahrradgabeln", "Fahrradrahmen", "Fahrradreifen", "Fahrradschläuche", "Laufräder", "Powermeter", "Sättel", "Sattelstützen", "Schaltung"]
 const ModuleItemCount = 10
 const ModuleSum = "150,00 €"
 const ModuleId = ["Module1", "Module2", "Module3", "Module4", "Module5", "Module6"]
@@ -31,22 +31,25 @@ export default {
     <div class="row mt-3">
       <div class="col">
         <ConfiguratorHeader />
-        <i class="bi bi-bicycle"></i>
-        <div class="row mt-3">
-          <ConfiguratorModule :ModuleId="ModuleId[0]" :ModuleGroup="ModuleGroup[0]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
-          <ConfiguratorModule :ModuleId="ModuleId[1]" :ModuleGroup="ModuleGroup[1]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
-          <ConfiguratorModule :ModuleId="ModuleId[2]" :ModuleGroup="ModuleGroup[3]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
+        <div class="row z-n1">
+          <bicycle />
         </div>
+        <div class="row mt-3 z-3">
+          <ConfiguratorModule :ModuleId="ModuleId[0]" :ModuleGroup="productData[0].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
+          <ConfiguratorModule :ModuleId="ModuleId[1]" :ModuleGroup="productData[1].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
+          <ConfiguratorModule :ModuleId="ModuleId[2]" :ModuleGroup="productData[3].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
+        </div>
+
         <div class="row mt-3">
-          <ConfiguratorModule :ModuleId="ModuleId[3]" :ModuleGroup="ModuleGroup[5]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
-          <ConfiguratorModule :ModuleId="ModuleId[4]" :ModuleGroup="ModuleGroup[10]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
-          <ConfiguratorModule :ModuleId="ModuleId[5]" :ModuleGroup="ModuleGroup[8]" :ModuleItemCount="ModuleItemCount"
-            :ModuleSum="ModuleSum" />
+          <ConfiguratorModule :ModuleId="ModuleId[3]" :ModuleGroup="productData[5].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
+          <ConfiguratorModule :ModuleId="ModuleId[4]" :ModuleGroup="productData[10].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
+          <ConfiguratorModule :ModuleId="ModuleId[5]" :ModuleGroup="productData[8].groupname"
+            :ModuleItemCount="ModuleItemCount" :ModuleSum="ModuleSum" />
         </div>
       </div>
       <ConfiguratorProductList />
