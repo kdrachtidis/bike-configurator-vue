808443f4c0c55e0aa7a130f93acf95ed209ebd78
Konstantinos Drachtidis <kdrachtidis@outlook.com>
2024-06-05 10:58:45 +0200

    Basic i18n plugin added

--- bc-app/src/components/ConfiguratorHeader.vue
+++ bc-app/src/components/ConfiguratorHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-md bg-light-subtle rounded">
         <div class="container-fluid">
-            <span class="navbar-brand">Mein Rennrad</span>
+            <span class="navbar-brand">{{ $translate('de.TemplateName') }}</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bikeType"
                 aria-controls="bikeType" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -15,9 +15,8 @@
                         <a class="nav-link" href="#">Zubehör</a>
                     </li>
                 </ul>
-                <form class="d-flex"><button type="button" class="btn btn-sm btn-outline-secondary" aria-label="Baugruppe
-                                    hinzufügen">Baugruppe
-                        hinzufügen</button></form>
+                <form class="d-flex"><button type="button" class="btn btn-sm btn-outline-secondary"
+                        aria-label="Baugruppe hinzufügen">{{ $translate('de.TemplateButtonAddComp') }}</button></form>
             </div>
         </div>
     </nav>
--- bc-app/src/main.js
+++ bc-app/src/main.js
@@ -4,5 +4,23 @@ import 'bootstrap/dist/js/bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18nPlugin from '/plugins/i18nPlugin'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(i18nPlugin, {
+    en: {
+        TemplateName: 'My Road Bike',
+        TemplateButtonAddComp: 'Add Components Group'
+    },
+    de: {
+        TemplateName: 'Mein Rennrad',
+        TemplateButtonAddComp: 'Baugruppe hinzufügen'
+    },
+    gr: {
+        TemplateName: 'Το αγωνιστικό μου',
+        TemplateButtonAddComp: 'Πρόσθεσε ομάδα'
+    }
+})
+
+app.mount('#app');
