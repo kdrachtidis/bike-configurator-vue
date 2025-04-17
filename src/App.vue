6f9bb57dca63e5cf976eddad4d0f80f44c63f06f
Konstantinos Drachtidis <kdrachtidis@outlook.com>
2025-04-17 11:06:18 +0200

    Switched to composition

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <script setup>
+import groupData from './assets/groups.json'
 import AppHeader from './components/ui/AppHeader.vue'
 import ConfiguratorHeader from './components/ui/ConfiguratorHeader.vue'
 import ConfiguratorProductList from './components/basket/ConfiguratorProductList.vue'
 import ConfiguratorModule from './components/module/ConfiguratorModule.vue'
-import bicycle from './components/ui/bicycle.vue'
-
-const ModuleItemCategory = ["Adapter-Kits", "Cleats & Schuhplatten", "Fahrradkassetten", "Fahrradketten", "Fahrradkurbeln", "Innenlager", "Kettenblätter", "Kettenführungen & Bashguards", "Kleinteile", "Nabenschaltung", "Pedale", "Riemen", "Riemenscheiben", "Singlespeed", "Verschleißsets"]
 
 const ModuleItemCount = 10
 const ModuleSum = "150,00 €"
-const ModuleId = ["Module1", "Module2", "Module3", "Module4", "Module5", "Module6"]
-
-</script>
-
-<script>
-import groupData from './assets/groups.json'
-
-export default {
-  data() {
-    return {
-      groups: groupData
-    }
-  }
-}
+const groups = groupData
 </script>
 
 <template>
--- src/components/module/ConfiguratorModule.vue
+++ src/components/module/ConfiguratorModule.vue
@@ -1,6 +1,4 @@
-<script>
-import { ref } from 'vue'
-
+<script setup>
 import groupmodulesData from '../../assets/groupmodules.json'
 
 import ConfiguratorModuleHeader from './ConfiguratorModuleHeader.vue'
@@ -9,42 +7,27 @@ import ConfiguratorModuleSubHeader from './ConfiguratorModuleSubHeader.vue'
 import ConfiguratorModuleItemCategory from './ConfiguratorModuleItemCategory.vue'
 import ConfiguratorModuleItemProduct from './ConfiguratorModuleItemProduct.vue'
 
-const sum = "150,00 €"
-const count = "2"
+const groupmodules = groupmodulesData
 
-export default {
-    components: {
-        ConfiguratorModuleHeader,
-        ConfiguratorModuleFooter,
-        ConfiguratorModuleSubHeader,
-        ConfiguratorModuleItemCategory,
-        ConfiguratorModuleItemProduct
-    },
-    props: {
-        ModuleId: String,
-        ModuleGroup: String,
-        ModuleItemCount: Number,
-        ModuleSum: String
-    },
-    data() {
-        return {
-            groupmodules: groupmodulesData
-        }
-    }
-}
+const props = defineProps({
+    ModuleId: String,
+    ModuleGroup: String,
+    ModuleItemCount: Number,
+    ModuleSum: String
+})
 </script>
 
 <template>
-        <section class="card border-secondary mb-3">
-            <ConfiguratorModuleHeader :Id="'#' + ModuleId" :Group="ModuleGroup" />
-            <ConfiguratorModuleSubHeader :ItemCount="ModuleItemCount" />
-            <div class="collapse show" :id="ModuleId">
-                <div class="card-body p-0 overflow-auto" style="height: 300px;">
-                    <ul v-for="groupmodule in groupmodules" class="list-group list-group-flush">
-                        <ConfiguratorModuleItemCategory :Category="groupmodule.name" />
-                    </ul>
-                </div>
+    <section class="card border-secondary mb-3">
+        <ConfiguratorModuleHeader :Id="'#' + ModuleId" :Group="ModuleGroup" />
+        <ConfiguratorModuleSubHeader :ItemCount="ModuleItemCount" />
+        <div class="collapse show" :id="ModuleId">
+            <div class="card-body p-0 overflow-auto" style="height: 300px;">
+                <ul v-for="groupmodule in groupmodules" class="list-group list-group-flush">
+                    <ConfiguratorModuleItemCategory :Category="groupmodule.name" />
+                </ul>
             </div>
-            <ConfiguratorModuleFooter :Sum="ModuleSum" />
-        </section>
+        </div>
+        <ConfiguratorModuleFooter :Sum="ModuleSum" />
+    </section>
 </template>
